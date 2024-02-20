6e6713f05aedda19da78092e2d1eb49585328787
Sharmila-Rajendran <139245927+Sharmila-Rajendran@users.noreply.github.com>
2024-02-21 00:17:56 +0530

    Update ConversationComponents.js

--- my-app/src/components/ConversationComponents.js
+++ my-app/src/components/ConversationComponents.js
@@ -1,91 +1,133 @@
-import styled from "styled-components";
-import { messagesList } from "../Data";
-import { SearchContainer, SearchInput } from "./ContactListComponents";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { messagesList } from '../Data';
 
 const Container = styled.div`
-display: flex ;
-flex-direction: column;
-height: 100%;
-flex: 2;
-background: #f6f7f8;
-`;
-const ProfileHeader=styled.div`
-    display: flex;
-    flex-direction: row;
-    background: #ededed;
-    padding: 15px;
-    align-items: center;
-    gap : 10px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex: 2;
+  background: #f6f7f8;
 `;
 
-const ProfileImage =styled.img`
-width: 40px;
-height: 40px;
-border-radius: 50%;
+const ProfileHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #ededed;
+  padding: 15px;
+  align-items: center;
+  gap: 10px;
 `;
 
-const ChatBox =styled.div`
-display: flex;
-background: #f0f0f0;
-padding: 10px;
-align-items: center;
-bottom : 0;
+const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
 `;
-
-const EmojiImage =styled.img`
-width:30px;
-height: 28px;
-opacity: 0.4;
-cursor: pointer;
-padding-left:10px;
-`;
-
 
 const MessageContainer = styled.div`
-display: flex;
-flex-direction: column;
-height: 100%;
-background:transparent;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 10px;
+  overflow-y: auto;
 `;
 
 const MessageDiv = styled.div`
-justify-content: ${(props) => (props.isYours ?'flex-end':'flex-start') };
-display:flex;
-margin: 5px 16px;
+  display: flex;
+  justify-content: ${(props) => (props.isYours ? 'flex-end' : 'flex-start')};
+  margin-bottom: 10px;
 `;
 
 const Message = styled.div`
-background: ${(props) => (props.isYours ?"lightgrey":"white" )};
-max-width:50%;
-color: #303030;
-border-radius:50px;
-padding: 8px 10px;
-font-size: 15px;
+  background: ${(props) => (props.isYours ? 'lightblue' : 'lightgrey')};
+  max-width: 70%;
+  padding: 8px 10px;
+  border-radius: 8px;
+  font-size: 15px;
 `;
 
-const ConversationComponent = () => {
-    return (
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-top: 1px solid #ccc;
+`;
+
+const Input = styled.input`
+  flex-grow: 1;
+  padding: 8px;
+  font-size: 15px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
+
+const Button = styled.button`
+  padding: 8px 16px;
+  font-size: 15px;
+  border-radius: 5px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+const FilesIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const ConversationComponents = () => {
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState(messagesList);
+
+  const handleMessageSend = () => {
+    if (inputText.trim() !== '') {
+      const newMessage = {
+        id: messages.length + 1,
+        messageType: 'TEXT',
+        text: inputText,
+        senderID: 0,
+        addedOn: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      };
+      setMessages([...messages, newMessage]);
+      setInputText('');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleMessageSend();
+    }
+  };
+
+  return (
     <Container>
-        <ProfileHeader>
+      <ProfileHeader>
         <ProfileImage src="/ProfileImage.jpg" />
         Sharmila
-        </ProfileHeader>
-        <MessageContainer>
-            {messagesList.map((messageData) => (
-                <MessageDiv isYours={messageData.senderID === 0}>
-                  <Message isYours={messageData.senderID === 0}>{[messageData.text]} </Message>
-                </MessageDiv>
-            ))}
-
-        </MessageContainer>
-        <ChatBox>
-            <EmojiImage src={"/data.svg"}/>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1.5em" width="1.5em" color="#696969" cursor="pointer" xmlns="http://www.w3.org/2000/svg"><path d="M14 7v1H8v6H7V8H1V7h6V1h1v6h6z"></path></svg>
-            <SearchContainer>
-                <SearchInput placeholder="Type a message"/>
-            </SearchContainer>
-        </ChatBox>
-        </Container>
-    );
+      </ProfileHeader>
+      <MessageContainer>
+        {messages.map((messageData) => (
+          <MessageDiv key={messageData.id} isYours={messageData.senderID === 0}>
+            <Message isYours={messageData.senderID === 0}>{messageData.text}</Message>
+          </MessageDiv>
+        ))}
+      </MessageContainer>
+      <InputContainer>
+        <Input
+          type="text"
+          placeholder="Type a message"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <Button onClick={handleMessageSend}>Send</Button>
+      </InputContainer>
+    </Container>
+  );
 };
-export default ConversationComponent;
+
+export default ConversationComponents;
